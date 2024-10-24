38f0d14d324045cb7a204a0883b70a0af81acd47
kenshiro8 <kenshiro.160818@gmail.com>
2024-10-24 18:51:13 +0900

    change clicked image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const imageArray = [
   { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
@@ -9,21 +9,29 @@ const imageArray = [
 ];
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(imageArray[0]);
+
+  const handleClick = (index) => {
+    setSelectedImage(imageArray[index]);
+  };
   return (
     <>
       <h1>Image gallery example</h1>
       <div className="full-img">
         <img
           className="displayed-img"
-          src={imageArray[0].src}
-          alt={imageArray[0].alt}
+          src={selectedImage.src}
+          alt={selectedImage.alt}
         />
         <div className="overlay"></div>
         <button className="dark">Darken</button>
       </div>
       <div className="thumb-bar">
         {imageArray.map((image, index) => (
-        <img key={index} src={image.src} alt={image.alt} />
+        <img key={index}
+          src={image.src}
+          alt={image.alt}
+          onClick={() => handleClick(index)}/>
         ))}
       </div>
     </>
