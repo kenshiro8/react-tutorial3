b82cc9c0900bb411ce3f8badd46eb92e1ebdf247
kenshiro8 <kenshiro.160818@gmail.com>
2024-10-24 18:00:57 +0900

    import react and add image link

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+
+const imageArray = [
+  { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+  { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+  { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+  { src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
+  { src: "images/pic5.jpg", alt: "Large moth on a leaf" }
+];
+
 export default function App() {
   return (
     <>
@@ -5,13 +15,17 @@ export default function App() {
       <div className="full-img">
         <img
           className="displayed-img"
-          src="images/pic1.jpg"
-          alt="Closeup of a human eye"
+          src={imageArray[0].src}
+          alt={imageArray[0].alt}
         />
         <div className="overlay"></div>
         <button className="dark">Darken</button>
       </div>
-      <div className="thumb-bar"></div>
+      <div className="thumb-bar">
+        {imageArray.map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} />
+        ))}
+      </div>
     </>
   );
 }
